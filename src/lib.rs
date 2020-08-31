117ac59e6f8a5d93cfdd09de731bb587c48430c2
Tmacy <tmacy89120@gmail.com>
2020-09-01 00:06:11 +0800

    commit one

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+//pub fn say(s: &str) -> String {
+//  println!("The Rust function say() received {}", s);
+//  let r = String::from("hello ");
+//  return r + s;
+//}
+pub fn solve(params: &str) -> String {
+    let ps: (f32, f32, f32) = serde_json::from_str(&params).unwrap();
+    let discriminant: f32 = (ps.1 * ps.1) - (4. * ps.0 * ps.2);
+    let mut solution: (f32, f32) = (0., 0.);
+    if discriminant >= 0. {
+        solution.0 = (((-1.) * ps.1) + discriminant.sqrt()) / (2. * ps.0);
+        solution.1 = (((-1.) * ps.1) - discriminant.sqrt()) / (2. * ps.0);
+        return serde_json::to_string(&solution).unwrap();
+    } else {
+        return String::from("not real numbers");
+    }
 }
